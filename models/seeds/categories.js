fe6db85dade79691f687d48171f30470d181a0f4
Flora2020 <rossignols1989@gmail.com>
2021-02-02 12:27:03 +0800

    feat: add category seeder and record seeder

---
+++ models/seeds/categories.js
@@ -0,0 +1,9 @@
+const categories = [
+  { name: '家居物業' },
+  { name: '交通出行' },
+  { name: '休閒娛樂' },
+  { name: '餐飲食品' },
+  { name: '其他' }
+]
+
+module.exports = categories
---
+++ models/seeds/records.js
@@ -0,0 +1,28 @@
+const records = [
+  {
+    name: '鹿皮巾拖把',
+    category: '家居物業',
+    date: '2021-01-07',
+    amount: 375
+  },
+  {
+    name: '蕃茄',
+    category: '餐飲食品',
+    date: '2021-01-14',
+    amount: 100
+  },
+  {
+    name: '智能腦控車',
+    category: '休閒娛樂',
+    date: '2021-02-01',
+    amount: 1590
+  },
+  {
+    name: '95無鉛汽油',
+    category: '交通出行',
+    date: '2021-01-23',
+    amount: 63
+  }
+]
+
+module.exports = records
