59a0ab93aec1ddaeee177c26d19e943accb0384c
Flora2020 <rossignols1989@gmail.com>
2021-03-11 15:29:17 +0800

    feat: add user route

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const home = require('./modules/home.js')
 const records = require('./modules/records.js')
+const users = require('./modules/users.js')
 
 const router = express.Router()
 
-router.use('/', home)
 router.use('/records', records)
+router.use('/users', users)
+router.use('/', home)
 
 module.exports = router
