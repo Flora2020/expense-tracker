2fecaa5ba4299f5a6cc3ec685974c41f1ebdc2ac
Flora2020 <rossignols1989@gmail.com>
2021-03-14 15:14:04 +0800

    style: extra semicolon

--- routes/modules/auth.js
+++ routes/modules/auth.js
@@ -3,7 +3,7 @@ const passport = require('passport')
 const router = express.Router()
 
 router.get('/facebook',
-  passport.authenticate('facebook', { scope: 'email' }));
+  passport.authenticate('facebook', { scope: 'email' }))
 
 router.get('/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
